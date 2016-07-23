500ddb3da6dc8909bec0df8ad9e6957575ab319f
Sunil Biswakarma <sun.aec@gmail.com>
2016-07-24 00:17:25 +0530

    shopping list and recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe';
 import { RecipeItemComponent } from './recipe-item.component';
+import { RecipeService } from '../recipe.service';
+
 @Component({
   moduleId: module.id,
   selector: 'rb-recipe-list',
@@ -8,15 +10,14 @@ import { RecipeItemComponent } from './recipe-item.component';
   directives: [RecipeItemComponent]
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[]=[
-    new Recipe('Masala Chai', 'Refreshing Tea', 'http://dassana.cdnrecipes.netdna-cdn.com//wp-content/uploads/2016/06/masala-tea-recipe.jpg', []),
-    new Recipe('Oats Recipe', 'Oats Refresher', 'http://dassana.cdnrecipes.netdna-cdn.com//wp-content/uploads/2016/06/oats-uttapam-recipe.jpg', [])
- ];
+  recipes: Recipe[]=[];
+  
   @Output() recipeSelected=new EventEmitter<Recipe>();
   
-  constructor() { }
+  constructor(private recipeService:RecipeService){}
   
   ngOnInit() {
+    this.recipes=this.recipeService.getRecipes();
   }
 
   onSelected(recipe:Recipe){
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe';
+import {Ingredient} from '../shared'
+@Injectable()
+export class RecipeService {
+private recipes: Recipe[]=[
+    new Recipe('Masala Chai', 'Refreshing Tea', 'http://dassana.cdnrecipes.netdna-cdn.com//wp-content/uploads/2016/06/masala-tea-recipe.jpg', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Pork Meat', 1)
+    ]),
+    new Recipe('Oats Recipe', 'Oats Refresher', 'http://dassana.cdnrecipes.netdna-cdn.com//wp-content/uploads/2016/06/oats-uttapam-recipe.jpg', [])
+ ];
+  constructor() { }
+  getRecipes(){
+    return this.recipes;
+  }
+}
